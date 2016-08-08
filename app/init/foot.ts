ea3785d5788fd2bc8e2208689cf45ad0ea53583b
topshadow <2121718893@qq.com>
2016-08-08 15:17:27 +0800

    添加bower.json

---
+++ app/init/foot.ts
@@ -0,0 +1,12 @@
+import {Component,Input} from '@angular/core';
+
+@Component({
+    selector:'foot',
+    template:`
+<h1>页脚</h1>`
+})
+export class Foot{
+    @Input()
+    public data;
+
+}
---
+++ app/init/index.ts
@@ -0,0 +1,5 @@
+export * from './page';
+export * from './page-component/page-component';
+export * from './nav';
+export * from './foot';
+export * from './toolbar';
