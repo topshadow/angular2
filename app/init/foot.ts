3583036be8ef9db0dace92c45b3f668e2bef462c
Administrator <wan19930624>
2016-08-08 16:58:24 +0800

    footer

--- app/init/foot.ts
+++ app/init/foot.ts
@@ -2,8 +2,11 @@ import {Component,Input} from '@angular/core';
 
 @Component({
     selector:'foot',
-    template:`
-<h1>页脚</h1>`
+    template:`<div class="footerBar">
+          <p>版权所有 © 2011-2016 上海旅烨网络科技有限公司，严禁抄袭复制</p>
+          <p>ICP备案号：沪ICP备15011558号</p>
+     </div>`,
+    styles:[' .footerBar{ background:#000; color: #fff; text-align: center; padding: 30px;}']
 })
 export class Foot{
     @Input()
