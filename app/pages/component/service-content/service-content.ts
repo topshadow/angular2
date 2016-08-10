40ae6a17237704eecfaa651bbd380e1ba35b90ff
Administrator <wan19930624>
2016-08-10 10:28:38 +0800

    product-list

--- app/pages/component/service-content/service-content.ts
+++ app/pages/component/service-content/service-content.ts
@@ -1,9 +1,14 @@
-import {Component,Input } from '@angular/core';
+import {Component,Input , ViewChild} from '@angular/core';
+import {CORE_DIRECTIVES} from '@angular/common';
+import {MODAL_DIRECTIVES, BS_VIEW_PROVIDERS,ModalDirective} from 'ng2-bootstrap/ng2-bootstrap';
+
 
 @Component({
     selector:`service-content`,
     templateUrl:`app/pages/component/service-content/service-content.html`,
-    styleUrls:['app/pages/component/service-content/service-content.css']
+    styleUrls:['app/pages/component/service-content/service-content.css'],
+    directives: [MODAL_DIRECTIVES, CORE_DIRECTIVES],
+    viewProviders:[BS_VIEW_PROVIDERS],
 })
 export class  ServiceContent{
     @Input()
@@ -11,5 +16,15 @@ export class  ServiceContent{
 
     get isEdit(){
         return window['isEdit'];
+    }
+
+    @ViewChild('childModal') public childModal: ModalDirective;
+
+    public showChildModal():void {
+        this.childModal.show();
+    }
+
+    public hideChildModal():void {
+        this.childModal.hide();
     }
 }
