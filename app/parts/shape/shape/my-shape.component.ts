a7b43807d8add841e4d7fb9bcb17f4a995e6f76c
topshadow <2121718893@qq.com>
2016-09-12 18:34:16 +0800

    my-shape

--- app/parts/shape/shape/my-shape.component.ts
+++ app/parts/shape/shape/my-shape.component.ts
@@ -2,5 +2,6 @@ import {Component,ElementRef} from '@angular/core';
 import {Router}  from '@angular/router';
 
 @Component({
-    selector:'my-shape'
+    selector:'my-shape',
+    templateUrl:'./my-shape.component.html'
 })
